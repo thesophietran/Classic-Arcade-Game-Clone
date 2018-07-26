45c6540ac9aae0c26a8e081b194241ef0afc6a78
Loan Tran <loantranvn810@gmail.com>
2018-07-26 18:12:01 -0500

    Desctructuring Class

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-// Enemies our player must avoid
+// Enemies Class
 class Enemy {
-    constructor() {
+    constructor({sprite = "images/enemy-bug.png", x = 0, y = 60, speed = 150} = {}) {
         // load an enemy image
-        this.sprite = 'images/enemy-bug.png';
+        this.sprite = sprite;
 
         // Setting the Enemy initial location
-        this.x = 0;
-        this.y = 60; 
+        this.x = x;
+        this.y = y; 
 
         // Setting the Enemy speed 
-        this.speed = 150;
+        this.speed = speed;
     }
 
     update(dt) {
@@ -23,7 +23,10 @@ class Enemy {
             this.x = 0; 
         }
 
-    // Handles collision with the Player
+        // Handles collision with the Player
+        // if (this.x === Player.x) {
+        //     console.log("COLLIDED");
+        // }
     }
 
     render() {
@@ -33,15 +36,13 @@ class Enemy {
 }
 
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player Class
 class Player {
-    constructor() {
-        this.sprite = 'images/char-boy.png';
-        this.x = 200;
-        this.y = 400;
-        this.speed = 100; 
+    constructor({sprite = 'images/char-boy.png', x = 200, y = 400, speed = 100} = {}) {
+        this.sprite = sprite;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;  
     }
 
     update(dt) {
@@ -58,7 +59,7 @@ class Player {
         this.speed = 100; 
     }
 
-    handleInput() {
+    handleInput(dt) {
         // move the Player
         switch(event.keyCode) {
             case 37:
@@ -71,22 +72,22 @@ class Player {
                 this.x += this.speed;
                 break; 
             case 40:
-                this.y += this.speed; 
+                this.y += this.speed;   
                 break;
         }
 
         // the player cannot move off screen
-        if (this.x >= 400) {
+        if (this.x > 400) {
             this.speed = 0; 
-            this.x = 399; 
+            this.x = 400; 
         } 
-        else if (this.x <= 0) {
+        else if (this.x < 0) {
             this.speed = 0; 
-            this.x = 1;
+            this.x = 0;
         }
-        else if (this.y >= 400) {
+        else if (this.y > 400) {
             this.speed = 0; 
-            this.y = 399;
+            this.y = 400;
         }
         else {
             this.speed = 100; 
@@ -103,7 +104,8 @@ class Player {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy()]; 
+// const allEnemies = [new Enemy({y:150}), new Enemy({y:220, speed:70})]; 
+var allEnemies = [new Enemy(), new Enemy({y:150, speed:50}), new Enemy({y:240, speed:20})]; 
 
 var player = new Player(); 
 
